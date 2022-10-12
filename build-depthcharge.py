306ca86dd4de853db9c281ebb423bb944bf16be1
Apacelus <46828271+apacelus@users.noreply.github.com>
2022-10-12 23:53:44 +0200

    Force resize

--- build-depthcharge.py
+++ build-depthcharge.py
@@ -204,8 +204,7 @@ def compress_image(img_mnt: str) -> None:
     print_status("Shrinking image")
 
     # Shrink image to actual size
-    bash(f"e2fsck -f {img_mnt}p2")  # Check filesystem
-    bash(f"resize2fs -M {img_mnt}p2")
+    bash(f"resize2fs -f -M {img_mnt}p2")
     block_count = int(bash(f"dumpe2fs -h {img_mnt}p2 | grep 'Block count:'")[12:].split()[0])
     actual_fs_in_bytes = block_count * 4096
     # the kernel part is always the same size -> sector amount: 131072 * 512 => 67108864 bytes
@@ -277,6 +276,7 @@ if __name__ == "__main__":
 
     # Unmount image to prevent tar error: "file changed as we read it"
     bash("umount -f /mnt/eupnea")
+    sleep(5)  # wait for umount to finish
     compress_image(image_props[1])
 
     bash(f"losetup -d {image_props[1]}")  # unmount image
