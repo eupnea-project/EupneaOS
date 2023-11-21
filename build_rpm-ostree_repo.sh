6f6475e65b64c58fce0259a15b27acb5db76a8f9
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-11-21 09:37:56 +0100

    Fresh clone worstation ostree configs
    Instead of keeping the ostree files directly in the repo, git clone them on build and copy the eupneaos config into the directory

--- build_rpm-ostree_repo.sh
+++ build_rpm-ostree_repo.sh
@@ -2,10 +2,16 @@
 
 dnf install -y ostree rpm-ostree
 
+# download the standard ostree configs
+git clone --branch="f39" --depth=1 https://pagure.io/workstation-ostree-config.git
+
+# copy eupneaos config into cloned ostree configs
+cp ./configs/ostree-eupneaos.yaml ./workstation-ostree-config/ostree-eupneaos.yaml
+
 mkdir -p /mnt/rpm-ostree-repo/repo /mnt/rpm-ostree-repo/cache
 
 # Initialize the OSTree repository
 ostree --repo=/mnt/rpm-ostree-repo/repo init --mode=archive
 
 # build the ostree repo
-rpm-ostree compose tree --repo=/mnt/rpm-ostree-repo/repo --cachedir=/mnt/rpm-ostree-repo/cache ./ostree-configs/fedora-eupneaos.yaml
+rpm-ostree compose tree --repo=/mnt/rpm-ostree-repo/repo --cachedir=/mnt/rpm-ostree-repo/cache ./workstation-ostree-config/ostree-eupneaos.yaml
