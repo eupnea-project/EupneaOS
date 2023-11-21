45fae085322e18efcbd65452d3a50b7e5482afe8
Apacelus <46828271+apacelus@users.noreply.github.com>
2023-11-21 09:57:06 +0100

    Fix typo

--- build_rpm-ostree_repo.sh
+++ build_rpm-ostree_repo.sh
@@ -14,4 +14,4 @@ mkdir -p /mnt/rpm-ostree-repo/repo /mnt/rpm-ostree-repo/cache
 ostree --repo=/mnt/rpm-ostree-repo/repo init --mode=archive
 
 # build the ostree repo
-rpm-ostree compose tree --repo=/mnt/rpm-ostree-repo/repo --cachedir=/mnt/rpm-ostree-repo/cache ./workstation-ostree-config/osfedora-eupneaos.yaml
+rpm-ostree compose tree --repo=/mnt/rpm-ostree-repo/repo --cachedir=/mnt/rpm-ostree-repo/cache ./workstation-ostree-config/fedora-eupneaos.yaml
